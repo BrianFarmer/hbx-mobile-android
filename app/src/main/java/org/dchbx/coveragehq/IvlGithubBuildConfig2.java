a460f28c7ff847544827bca6b543b46a06d5d25e
Brian Farmer <plasticwhale@yahoo.com>
2017-07-17 12:17:22 -0400

    bunch of ridp

---
+++ app/src/main/java/org/dchbx/coveragehq/IvlGithubBuildConfig2.java
@@ -0,0 +1,65 @@
+package org.dchbx.coveragehq;
+
+class IvlBuildConfig2 extends EnrollConfigBase {
+
+    private static ServerConfiguration serverConfiguration = null;
+
+    public ServerConfiguration getServerConfiguration() {
+        if (serverConfiguration != null){
+            return serverConfiguration;
+        }
+        serverConfiguration = new ServerConfiguration();
+
+        serverConfiguration.dataInfo = new ServerConfiguration.HostInfo();
+        serverConfiguration.dataInfo.host = "enroll-mobile2.dchbx.org";
+        serverConfiguration.dataInfo.scheme = "https";
+        serverConfiguration.dataInfo.port = 443;
+
+
+        serverConfiguration.loginInfo = new ServerConfiguration.HostInfo();
+
+        serverConfiguration.loginInfo.host = "hbx-mobile2-preprod.dchbx.org";
+        serverConfiguration.loginInfo.scheme = "https";
+        serverConfiguration.loginInfo.port = 443;
+
+        /*
+        serverConfiguration.loginInfo.host = "mobile.dcmic.org";
+        serverConfiguration.loginInfo.scheme = "http";
+        serverConfiguration.loginInfo.port = 3003;
+        */
+
+
+        //serverConfiguration.employerListPath = "api/v1/mobile_api/employers_list";
+        serverConfiguration.loginPath = "login";
+        serverConfiguration.endpointsPath = "endpoints";
+        return serverConfiguration;
+    }
+
+    @Override
+    public int getTimeoutCountdownSeconds() {
+        return 30;
+    }
+
+    //
+    // This is the number of seconds the have to pass before the user gets
+    // a dialog telling them that the session is about to timeout.
+    //
+    @Override
+    public int getSessionTimeoutSeconds() {
+        return 14*60;
+    }
+
+    @Override
+    public String getVersion() {
+        return "preprod";
+    }
+
+    @Override
+    public BrokerWorkerConfig.DataSource DataSource() {
+        return BrokerWorkerConfig.DataSource.MobileServer;
+    }
+
+    public String getUrl() {
+        return serverConfiguration.loginInfo.host;
+    }
+}
---
+++ app/src/main/java/org/dchbx/coveragehq/ridp/AcctSystemFoundYouAceds.java
@@ -0,0 +1,61 @@
+package org.dchbx.coveragehq.ridp;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import org.dchbx.coveragehq.AcctActivity;
+import org.dchbx.coveragehq.R;
+import org.dchbx.coveragehq.StateManager;
+import org.dchbx.coveragehq.databinding.AcctSystemFoundYouBinding;
+import org.dchbx.coveragehq.models.account.Account;
+
+/*
+    This file is part of DC.
+
+    DC Health Link SmallBiz is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DC Health Link SmallBiz is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DC Health Link SmallBiz.  If not, see <http://www.gnu.org/licenses/>.
+    This statement should go near the beginning of every source file, close to the copyright notices. When using the Lesser GPL, insert the word “Lesser” before “General” in all three places. When using the GNU AGPL, insert the word “Affero” before “General” in all three places.
+*/
+public class AcctSystemFoundYou extends AcctActivity {
+    public static StateManager.UiActivity uiActivity = new StateManager.UiActivity(AcctSystemFoundYou.class);
+    private ImageButton continueButton;
+
+    private AcctSystemFoundYouBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.acct_system_found_you);
+    }
+
+    @Override
+    protected void populate(final Account account) {
+    }
+
+    public void pleaseLogInClicked(){
+        Toast toast = Toast.makeText(this, "please sign in clicked", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void reviewYourAnswersClicked(){
+        Toast toast = Toast.makeText(this,"reviewYourAnswersClicked in clicked", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void comeBackLaterClicked(){
+        Toast toast = Toast.makeText(this,"come back later clicke", Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
